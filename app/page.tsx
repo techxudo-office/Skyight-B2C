31fbf89d00820d033503a54972ecf328f6748cff
hashamuddin1 <hashamlaptop@gmail.com>
2025-05-29 17:37:01 +0500

    add images

--- app/page.tsx
+++ app/page.tsx
@@ -20,11 +20,39 @@ export default function HomePage() {
   })
 
   const trendingDestinations = [
-    { city: "Paris", country: "France", price: "$299", image: "/placeholder.svg?height=200&width=300", rating: 4.8 },
-    { city: "Tokyo", country: "Japan", price: "$599", image: "/placeholder.svg?height=200&width=300", rating: 4.9 },
-    { city: "New York", country: "USA", price: "$399", image: "/placeholder.svg?height=200&width=300", rating: 4.7 },
-    { city: "London", country: "UK", price: "$349", image: "/placeholder.svg?height=200&width=300", rating: 4.6 },
-  ]
+    {
+      city: "Paris",
+      country: "France",
+      price: "$299",
+      image:
+        "https://images.unsplash.com/photo-1502602898657-3e91760cbb34?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cGFyaXN8ZW58MHx8MHx8fDA%3D",
+      rating: 4.8,
+    },
+    {
+      city: "Tokyo",
+      country: "Japan",
+      price: "$599",
+      image:
+        "https://images.unsplash.com/photo-1540959733332-eab4deabeeaf?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8dG9reW98ZW58MHx8MHx8fDA%3D",
+      rating: 4.9,
+    },
+    {
+      city: "New York",
+      country: "USA",
+      price: "$399",
+      image:
+        "https://images.unsplash.com/photo-1485871981521-5b1fd3805eee?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8bmV3JTIweW9ya3xlbnwwfHwwfHx8MA%3D%3D",
+      rating: 4.7,
+    },
+    {
+      city: "London",
+      country: "UK",
+      price: "$349",
+      image:
+        "https://images.unsplash.com/photo-1529655683826-aba9b3e77383?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8bG9uZG9ufGVufDB8fDB8fHww",
+      rating: 4.6,
+    },
+  ];
 
   const handleSearch = () => {
     const params = new URLSearchParams(searchData)
@@ -90,10 +118,10 @@ export default function HomePage() {
                     Departure
                   </label>
                   <div className="relative">
-                    <CalendarDays className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" />
+                    {/* <CalendarDays className="absolute left-3 top-3 h-4 w-4 text-muted-foreground" /> */}
                     <Input
                       type="date"
-                      className="pl-10"
+                      className="pl-5"
                       value={searchData.departure}
                       onChange={(e) =>
                         setSearchData({
