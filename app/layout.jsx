bbefebf811cd8b5fe255e3641f11a6374133d9dc
M.Saad Sohail <saadsohail232005@gmail.com>
2025-06-16 18:43:50 +0500

    Feat: cicd configuration

--- app/layout.jsx
+++ app/layout.jsx
@@ -16,6 +16,7 @@ const metadata = {
 };
 
 export default function RootLayout({ children }) {
+  console.log("CICD TEST 1");
   return (
     <html lang="en" suppressHydrationWarning>
       <body className={inter.className}>
