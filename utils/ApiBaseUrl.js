f4989c69adf49a2347328b343c102015a8133192
M.Saad Sohail <saadsohail232005@gmail.com>
2025-07-14 15:47:16 +0500

    Feat: live base url added

--- utils/ApiBaseUrl.js
+++ utils/ApiBaseUrl.js
@@ -1,3 +1,3 @@
-const BASE_URL = "http://localhost:3002";
-// const BASE_URL = "https://api.skyight.com";
+// const BASE_URL = "http://localhost:3002";
+const BASE_URL = "https://client.skyight.com";
 export { BASE_URL };
